54b9e43f5b6d284b4cdedc4a738a11490c14d5cf
sandyr73 <52935026+sandyr73@users.noreply.github.com>
2019-07-16 22:00:58 +0200

    modificado con rutas

--- src/front.php
+++ src/front.php
@@ -1,2 +1,45 @@
 <?php
-include('view/home.html');
+
+ini_set("display_errors", true);
+
+$ruta = $_SERVER["REQUEST_URI"];
+
+
+switch ($ruta){
+	case "/es/home":
+		include "view/home.html";
+		break;
+	
+	case "/es/login":
+		include "view/login.html";
+		break;
+
+	case "/es/panties":
+		include "view/panties.html";
+		break;
+
+	case "/es/legal":
+		include "view/legal.html";
+		break;
+
+	case "/es/swimsuit":
+		include "view/swimsuit.html";
+		break;
+
+	case "/es/bras":
+		include "view/bras.html";
+		break;
+
+	case "/es/contacto":
+		include "view/contacto.html";
+		break;
+
+	default:
+		include "view/error.html";
+		break;
+
+
+
+}
+
+?>
