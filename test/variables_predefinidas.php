1e3c37597971e3f63d8ca4b21d95d38f7aa0d6dc
Carmen Custodio <chantra132@gmail.com>
2019-09-03 18:25:04 +0200

    subida de gitignore y variables predefinidas.

---
+++ test/variables_predefinidas.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<h3> $_GET</h3>
+	<pre>
+		<?php
+		var_dump($_GET);
+		?>
+	</pre>
+	<h3> $_COOKIE</h3>
+	<pre>
+		<?php
+		var_dump($_COOKIE);
+		?>
+	</pre>
+	<h3> $_POST</h3>
+	<pre>
+		<?php
+		var_dump($_POST);
+		?>x
+	</pre>
+	<h3> $_FILES</h3>
+	<pre>
+		<?php
+		var_dump($_FILES);
+		?>
+	</pre>
+	<h3> $_ENV</h3>
+	<pre>
+		<?php
+		var_dump($_ENV);
+		?>
+	</pre>
+	<h3> $_REQUEST</h3>
+	<pre>
+		<?php
+		var_dump($_REQUEST);
+		?>
+	</pre>
+	<h3> $_SESSION</h3>
+	<pre>
+		<?php
+		var_dump($_SESSION);
+		?>
+	</pre>
+	
+</body>
+</html>
