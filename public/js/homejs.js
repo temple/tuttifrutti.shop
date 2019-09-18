55c3c90701cf9fb06cbda3e557c21e9b4394250f
Carmen Custodio <chantra132@gmail.com>
2019-09-18 20:34:42 +0200

    Modificaciones en archivos css y JS

--- public/js/homejs.js
+++ public/js/homejs.js
@@ -1,15 +1,16 @@
-/*var images = [ 
-    '/image/chica1.png',
-    '/image/chica2.png',
-    '/image/chica3.png'
+
+var images = [ 
+    '../image/chica1.png',
+    '../image/chica2.png',
+    '../image/chica3.png'
 ];
 var cuenta = 0; // dice el número de cuenta de cambios de imagen
 
 function cambiarImagen(){
   var imagen_actual = cuenta % images.length;
-  /*document.querySelector('main').style.backgroundImage="url('"+images[imagen_actual]+"')";*/
- /* var new_image =images[imagen_actual];
-  document.querySelector('main img.producto').setAttribute('src',new_image  );
+  document.querySelector('main').style.backgroundImage="url('"+images[imagen_actual]+"')";
+  var new_image =images[imagen_actual];
+  document.querySelector('main').setAttribute('src',new_image  );
   cuenta++; 
 }
 window.setInterval(cambiarImagen,3000);
@@ -17,14 +18,14 @@ $(document).ready(function(){
 $("#slide_nav_button").click(function(){
 $('#slide_menu').animate({width:'toggle'},300);
 });
-});*/
+});
 /*cambio color logo*/
 /*
 var imagen = document.getElementById("logo");
 var new_image = 
 console.log (imagen);
 imagen.addEventListener("mouseover");
-*/
+
 window.addEventListener('load', iniciar);
 
 function iniciar() {
@@ -46,3 +47,4 @@ function mouseout() {
 function returnimage() {
   var imagen = document.getElementById('logo').setAttribute('src',"/image/logohome.png");
 }
+*/
