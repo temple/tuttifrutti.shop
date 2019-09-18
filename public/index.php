fcb4cd48b7211ce5b106d4c25012d2609ffb7d2a
Carmen Custodio <chantra132@gmail.com>
2019-09-18 20:37:54 +0200

    Creación de estructura MVC en archivos y modificación

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
-
-require_once __DIR__.'/../vendor/autoload.php';
+// Activación del reporte de todos los errores y mensajes informativos
 error_reporting(E_ALL);
+// 'display_errors' es el "flag" que activa (1) o desactiva (0) la visualización en pantalla del reporte
 ini_set('display_errors', 1);
+require_once __DIR__.'/../src/bootstrap.php';
 
-$front = new FrontController();
-$front->run();
-
+$reflector = new \ReflectionClass(APP);
+$app = $reflector->newInstance();
+if ($app instanceof ApplicationInterface)
+	$app->run();
+exit;
